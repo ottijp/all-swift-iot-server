0bac5b9711b44799e68248794506d50667fe0328
Satoshi SAKAO <ottijp@users.noreply.github.com>
2019-01-14 21:41:37 +0900

    [change] DBをSQLiteからPostgreSQLへ変更

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -17,17 +17,24 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    // Configure a PostgreSQL database
+    guard let databaseUrl = Environment.get("DATABASE_URL") else {
+        fatalError("DATABASE_URL needed")
+    }
+    guard let pgConfig = PostgreSQLDatabaseConfig(url: databaseUrl, transport: .unverifiedTLS) else {
+        fatalError("Invalid DATABASE_URL format")
+    }
+    let pg = PostgreSQLDatabase(config: pgConfig)
 
-    /// Register the configured SQLite database to the database config.
+    // Register the configured PostgreSQL database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.enableLogging(on: .psql)
+    databases.add(database: pg, as: .psql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .psql)
     services.register(migrations)
 
 }
